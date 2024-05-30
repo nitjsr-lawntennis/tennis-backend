7e0595f8642400fc816b61a0945c13f09ba1522f
srinadhnarne <120965294+srinadhnarne@users.noreply.github.com>
2024-05-30 23:33:34 +0530

    Update tournamentController.js

--- controllers/tournamentController.js
+++ controllers/tournamentController.js
@@ -61,7 +61,7 @@ export const deleteTournamentContoller = async (req,res)=>{
                                 .select('organiser')
                                 .populate('organiser')
                                 .select("email");
-        if(existing?.organiser.email===user?.email||user.role===1){
+        if(user.role===1||existing?.organiser?.email===user?.email){
             await tournamentModel.findByIdAndDelete(req.params.tid);
             await matchModel.deleteMany({tournament:req.params.tid});
             res.status(200).send({
